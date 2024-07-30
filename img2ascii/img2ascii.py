ca5e31e5da39f448b38505ff8d83f4eb3f69034f
davimgeo <156332851+davimgeo@users.noreply.github.com>
2024-07-30 13:24:06 -0300

    Update img2ascii.py

--- img2ascii/img2ascii.py
+++ img2ascii/img2ascii.py
@@ -1,37 +1,47 @@
 import cv2
+import numpy as np
 
-#ascii_density = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '
-ascii_density = " .:-=+*#%@"
+# ASCII density string
+#ascii_density = " .:-=+*#%@"
+ascii_density = " .:coPO?@■"
 ascii_image = ''
 
-image_path = '/home/malum/Desktop/coding_tests/img2ascii/images/lumahlinda.jpg'
-image = cv2.imread(image_path)
+try:
+    # Read the image
+    image = cv2.imread('/home/davi/Desktop/coding_tests/img2ascii/images/lain.jpg')
+    # Convert the image to grayscale
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+except:
+    print("Could not find the file. Please verify file path")
+    exit()
 
-#Turn the image into gray scale
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+# Get the dimentions of the image
 height, width = image.shape
 
-new_height = 100
-new_width = 200
+# Factor to scale down the image
+n = 2
 
-#Resizing the image for the new height and width
+# Calculate the new dimensions
+new_height = int(height / n)
+new_width = int(width / n)
+
+# Rezise image while maintaining the aspect ratio
 image = cv2.resize(image, (new_width, new_height))
 
-#Gets the correspondent ascii index for a brightness values
+# Get the corresponding ASCII character for a given brightness value
 def respective_ascii(current_brightness):
-    n = round(255 / (len(ascii_density) - 1), 2)
-    return ascii_density[int(current_brightness / n)] 
+    n = 255 / (len(ascii_density) - 1)
+    return ascii_density[int(np.ceil(current_brightness / n))] 
 
-#Iterate for each brightness value and gets correspondent ascii char
+# Create the ASCII image
 for i in range(new_height):
     for j in range(new_width):
         brightness = image[i][j]
         ascii_image += respective_ascii(brightness)
     ascii_image += '\n'
 
-#Generate a txt of the ascii image
-# with open("ascii_image.txt", 'w') as f:
-#      f.write(ascii_image)
-
+# Write the ASCII art to a text file
+with open("ascii_image.txt", 'w', encoding='utf-8') as f:
+    f.write(ascii_image)
+    
 print(ascii_image)
